33ca1c63f94d33b31b05098368cb698936a4a569
Paul's Repository <cnubigdata@gmail.com>
2025-05-26 09:46:16 +0900

    Update aoai_lab_5.py

--- aoai_lab_5.py
+++ aoai_lab_5.py
@@ -26,3 +26,18 @@ data = {
     ],
     "chat_input": "국가 AI 역량 강화방안의 추진 방향과 차별점을 설명해줘. 한글로"
 }
+
+json_data = json.dumps(data, ensure_ascii=False)
+
+# Request REST API
+response = requests.post(endpoint_url, headers=headers, data=json_data)
+
+print("Response Code:", response.status_code)
+
+try:
+    response_json = response.json()
+    print("Response:")
+    print(json.dumps(response_json, indent=4, ensure_ascii=False))
+except json.JSONDecodeError:
+    print("JSON Decoding Fail:")
+    print(response.text)
